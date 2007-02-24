2aa741055b26ab246f99254d7189d496c03567b0
dirk.vdb <dirk.vdb@46771ab1-372a-0410-af83-a75959933f1e>
2007-02-24 20:15:32 +0000

    Added option for generating a filmstrip overlay Made seek percentage configurable

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -7,24 +7,35 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+	try
+	{
+	
 	if (argc == 2)
 	{
 		if (0 == strcmp(argv[1], "-h"))
 		{
-			cout << "Usage: ffmpegthumbnailer: infile outfile size" << endl;
+			cout << "Usage: ffmpegthumbnailer: infile outfile size [overlayfilmstrip seekpercentage]" << endl;
 			return 0;
 		}
 	}
-	else if (argc != 4)
+	else if (argc == 4)
 	{
-		cout << "Usage: ffmpegthumbnailer: infile outfile size" << endl;
+		VideoThumbnailer videoThumbailer(argv[1]);
+		videoThumbailer.generateThumbnail(argv[2], atoi(argv[3]), 1, 10);
+	}
+	else if (argc == 6)
+	{
+		VideoThumbnailer videoThumbailer(argv[1]);
+		videoThumbailer.generateThumbnail(argv[2], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+	}
+	else
+	{
+		cout << "Usage: ffmpegthumbnailer: infile outfile size [overlayfilmstrip seekpercentage]" << endl;
 		return -1;
 	}
 	
-	try
-	{
-		VideoThumbnailer videoThumbailer(argv[1]);
-		videoThumbailer.generateThumbnail(argv[2], atoi(argv[3]));
+	
+		
 	}
 	catch (Exception& e)
 	{
