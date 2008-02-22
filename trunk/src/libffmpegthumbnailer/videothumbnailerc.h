ccb84baa3ef9603a9542012a754f90d7dc7905d9
dirk.vdb <dirk.vdb@46771ab1-372a-0410-af83-a75959933f1e>
2008-02-22 01:27:06 +0000

    "added comments to interface file"

--- trunk/src/libffmpegthumbnailer/videothumbnailerc.h
+++ trunk/src/libffmpegthumbnailer/videothumbnailerc.h
@@ -26,29 +26,35 @@ extern "C"
     
 struct video_thumbnailer
 {
-    int         thumbnail_size;
-    int         seek_percentage;
-    int         overlay_film_strip;
-    int         workaround_bugs;
+    int         thumbnail_size;     /* default = 128 */
+    int         seek_percentage;    /* default = 10 */
+    int         overlay_film_strip; /* default = 0 */
+    int         workaround_bugs;    /* default = 0 */
     
-    void*       thumbnailer;
+    void*       thumbnailer;        /* for internal use only */
 };
 
 struct png_data
 {
-    uint8_t*    png_data_ptr;
-    int         png_data_size;
+    uint8_t*    png_data_ptr;       /* points to the png data after call to generate_thumbnail_to_buffer */
+    int         png_data_size;      /* contains the size of the png data after call to generate_thumbnail_to_buffer */
     
-    void*       internal_data;
+    void*       internal_data;      /* for internal use only */
 };
 
+/* intitialize video_thumbnailer structue */
 void thumbnailer_init(video_thumbnailer* thumbnailer);
+/* destroy video_thumbnailer structue */
 void thumbnailer_destroy(video_thumbnailer* thumbnailer);
 
+/* intitialize png_data structue */
 void png_data_init(png_data* data);
+/* destroy png_data structue */
 void png_data_destroy(png_data* data);
 
+/* generate thumbnail from video file (movie_filename), png data is stored in generated_png_data struct */
 void generate_thumbnail_to_buffer(video_thumbnailer* thumbnailer, const char* movie_filename, png_data* generated_png_data);
+/* generate thumbnail from video file (movie_filename), png is written to output_fileName on disk*/
 void generate_thumbnail_to_file(video_thumbnailer* thumbnailer, const char* movie_filename, const char* output_fileName);
 
 #ifdef __cplusplus
