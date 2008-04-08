ffbb41d3cb07761ce04c269ab523a79fa4ad03d9
dirk.vdb <dirk.vdb@46771ab1-372a-0410-af83-a75959933f1e>
2008-04-08 18:42:27 +0000

    Applied patch from Alexis Ballier to make ffmegthumbnailer compile against latest ffmpeg revisions

--- src/libffmpegthumbnailer/moviedecoder.h
+++ src/libffmpegthumbnailer/moviedecoder.h
@@ -25,17 +25,25 @@
 #include <vector>
 
 extern "C" {
+#if HAVE_LIBAVCODEC_AVCODEC_H
+#include <libavcodec/avcodec.h>
+#else
 #include <ffmpeg/avcodec.h>
+#endif
+#if HAVE_LIBAVFORMAT_AVFORMAT_H
+#include <libavformat/avformat.h>
+#else
 #include <ffmpeg/avformat.h>
+#endif
 }
 
 struct VideoFrame
 {
-	int width;
-	int height;
-	int lineSize;
-	
-	std::vector<uint8_t> frameData;
+    int width;
+    int height;
+    int lineSize;
+    
+    std::vector<uint8_t> frameData;
 };
 
 class MovieDecoder
@@ -46,11 +54,11 @@ public:
     
     std::string getCodec();
     void seek(int timeInSeconds);
-	void decodeVideoFrame();
-	void getScaledVideoFrame(int scaledSize, VideoFrame& videoFrame);
+    void decodeVideoFrame();
+    void getScaledVideoFrame(int scaledSize, VideoFrame& videoFrame);
 
-	int getWidth();
-	int getHeight();    
+    int getWidth();
+    int getHeight();    
     int getDuration();
     
 private:
@@ -58,7 +66,7 @@ private:
     void initializeVideo();
     void destroy();
     
-	bool decodeVideoPacket(AVPacket& packet);
+    bool decodeVideoPacket(AVPacket& packet);
     bool getVideoPacket(AVPacket& packet);
     void convertAndScaleFrame(int format, int scaledSize, int& scaledWidth, int& scaledHeight);
     void createAVFrame(AVFrame** avFrame, int width, int height, int format);
@@ -70,8 +78,8 @@ private:
     AVCodecContext*         m_pVideoCodecContext;
     AVCodec*                m_pVideoCodec;
     AVStream*               m_pVideoStream;
-	AVFrame*				m_pFrame;
-	bool					m_FormatContextWasGiven;
+    AVFrame*                m_pFrame;
+    bool                    m_FormatContextWasGiven;
 };
 
 #endif
