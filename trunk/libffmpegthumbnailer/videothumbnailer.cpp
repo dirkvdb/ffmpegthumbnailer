2665d2605911b6380feb1a5d54660a5e3a983f02
dirk.vdb <dirk.vdb@46771ab1-372a-0410-af83-a75959933f1e>
2009-02-06 12:13:29 +0000

    Fixed a bug in histogram generation

--- trunk/libffmpegthumbnailer/videothumbnailer.cpp
+++ trunk/libffmpegthumbnailer/videothumbnailer.cpp
@@ -344,8 +344,8 @@ void VideoThumbnailer::generateHistogram(const VideoFrame& videoFrame, Histogram
         for (int j = 0; j < videoFrame.width * 3; j += 3)
         {
             ++histogram.r[videoFrame.frameData[pixelIndex + j]];
-            ++histogram.g[videoFrame.frameData[pixelIndex + j] + 1];
-            ++histogram.b[videoFrame.frameData[pixelIndex + j] + 2];
+            ++histogram.g[videoFrame.frameData[pixelIndex + j + 1]];
+            ++histogram.b[videoFrame.frameData[pixelIndex + j + 2]];
         }
     }
 }
