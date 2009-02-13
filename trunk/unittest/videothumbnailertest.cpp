c79de43c0beec650baa4fad3f899cb63fc39e581
dirk.vdb <dirk.vdb@46771ab1-372a-0410-af83-a75959933f1e>
2009-02-13 07:13:12 +0000

    Removed ffmpegthumbnailer dir because it has the same name as the output binary and this causes errors

--- trunk/unittest/videothumbnailertest.cpp
+++ trunk/unittest/videothumbnailertest.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
 #include <algorithm>
 
+#include <vector>
+
 #include "libffmpegthumbnailer/videothumbnailer.h"
+
+using namespace std;
 
 class VideoThumbnailerTest : public testing::Test
 {
@@ -19,6 +23,11 @@ class VideoThumbnailerTest : public testing::Test
     {
         videoThumbnailer.generateHistogram(frame, histogram);
     }
+    
+    int getBestThumbnailIndex(vector<VideoFrame>& videoFrames, const vector<Histogram<int> >& histograms)
+    {
+        return videoThumbnailer.getBestThumbnailIndex(videoFrames, histograms);
+    }    
 
     Histogram<int> histogram;
     VideoFrame frame;
@@ -57,4 +66,44 @@ TEST_F(VideoThumbnailerTest, CreateHistogramWhiteFrame)
         EXPECT_EQ(0, histogram.g[i]);
         EXPECT_EQ(0, histogram.b[i]);
     }
+}
+
+TEST_F(VideoThumbnailerTest, FrameSelection)
+{
+    vector<VideoFrame> videoFrames;
+    vector<Histogram<int> > histograms;
+    
+    VideoFrame frame1(5, 5, 15);
+    fill(frame1.frameData.begin(), frame1.frameData.end(), 255);
+    
+    VideoFrame frame2(5, 5, 15);
+    fill(frame2.frameData.begin(), frame2.frameData.end(), 0);
+    
+    VideoFrame frame3(5, 5, 15);
+    fill(frame3.frameData.begin(), frame3.frameData.end(), 128);
+    
+    Histogram<int> hist1;
+    hist1.r[255] = 25;
+    hist1.g[255] = 25;
+    hist1.b[255] = 25;
+    
+    Histogram<int> hist2;
+    hist2.r[0] = 25;
+    hist2.g[0] = 25;
+    hist2.b[0] = 25;
+    
+    Histogram<int> hist3;
+    hist3.r[128] = 25;
+    hist3.g[128] = 25;
+    hist3.b[128] = 25;
+    
+    videoFrames.push_back(frame1);
+    videoFrames.push_back(frame2);
+    videoFrames.push_back(frame3);
+    
+    histograms.push_back(hist1);
+    histograms.push_back(hist2);
+    histograms.push_back(hist3);
+    
+    EXPECT_EQ(getBestThumbnailIndex(videoFrames, histograms), 1);
 }
