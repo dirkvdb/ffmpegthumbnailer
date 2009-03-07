34f34b3da7a775385427fd0b2c51500a16830833
dirk.vdb <dirk.vdb@46771ab1-372a-0410-af83-a75959933f1e>
2009-03-07 00:59:07 +0000

    Scripts added for automatic release building

---
+++ MakeRelease.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+version=""
+project="ffmpegthumbnailer"
+password=$1
+
+if [ "$password" == "" ]
+then
+	echo "No password for Google Code supplied"
+	exit 1
+fi
+
+echo "$password"
+
+#get version number
+version=`cat configure.ac | grep AC_INIT | cut -d[ -f3 | cut -d] -f1`
+
+#build the code
+builddir="out-$version"
+rm -rf $builddir
+mkdir -p $builddir
+cd $builddir
+../configure
+if [ $? != 0 ]
+then
+	echo "Configure failed"
+	exit 1
+fi
+
+make check
+if [ $? != 0 ]
+then
+	echo "Make check failed"
+	exit 1
+fi
+
+./unittests
+if [ $? != 0 ]
+then
+	echo "Unittests did not succeed"
+	exit 1
+fi
+
+make distcheck
+if [ $? != 0 ]
+then
+	echo "Make failed"
+	exit 1
+fi
+
+cd ..
+
+#upload archive to google
+python googlecode_upload.py -s "Release $version" -p $project -u dirk.vdb -w $password -l "Featured,Type-Source,OpSys-Linux"
+
+#create a tag
+#svn copy https://ffmpegthumbnailer.googlecode.com/svn/trunk https://ffmpegthumbnailer.googlecode.com/svn/tags/$project-$version -m "Tag of release $version"
