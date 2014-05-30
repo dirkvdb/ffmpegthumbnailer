e34303146a7ee948a064580958ba499da2683f97
dirk.vdb <dirk.vdb@46771ab1-372a-0410-af83-a75959933f1e>
2014-05-30 09:56:20 +0000

    Tag of release

---
+++ tags/2.0.9/unittest/histogramtest.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "gtest/gtest.h"
+#include "libffmpegthumbnailer/histogram.h"
+
+namespace ffmpegthumbnailer
+{
+
+TEST(HistogramTest, CreationInt)
+{
+    Histogram<int> hist;
+    
+    for (int i = 0; i < 255; ++i)
+    {
+        EXPECT_EQ(0, hist.r[i]);
+        EXPECT_EQ(0, hist.g[i]);
+        EXPECT_EQ(0, hist.b[i]);
+    }
+}
+
+TEST(HistogramTest, CreationFloat)
+{
+    Histogram<float> hist;
+    
+    for (int i = 0; i < 255; ++i)
+    {
+        EXPECT_EQ(0.0, hist.r[i]);
+        EXPECT_EQ(0.0, hist.g[i]);
+        EXPECT_EQ(0.0, hist.b[i]);
+    }
+}
+
+}
